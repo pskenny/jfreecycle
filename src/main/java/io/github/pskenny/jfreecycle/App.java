ab60a7ba53ae7eb8167f3254e31b8a2a33924ab3
Paul Kenny <paul.s.kenny@gmail.com>
2020-08-17 22:46:50 +0100

    Parse table data to Post object

--- src/main/java/io/github/pskenny/jfreecycle/App.java
+++ src/main/java/io/github/pskenny/jfreecycle/App.java
@@ -1,14 +1,15 @@
 package io.github.pskenny.jfreecycle;
 
 import java.io.IOException;
-
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
- * Freecycle commandline utility
+ * Freecycle commandline utility. Super duper fragile to changes on Freecycle. G'luck.
  */
 public class App {
 
@@ -17,13 +18,44 @@ public class App {
                 .append("/posts/offer").toString();
         try {
             Document doc = Jsoup.connect(url).get();
-            Element content = doc.getElementById("group_posts_table");
-            Elements links = content.getElementsByTag("tr");
+            Element table = doc.getElementById("group_posts_table");
+            Elements tableRow = table.getElementsByTag("tr");
 
-            links.forEach(x ->
-                // Remove " See details" from text and print to stdout
-                System.out.println(x.text().subSequence(0, x.text().length() - 12))
-            );
+            SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy", Locale.ENGLISH);
+
+            tableRow.forEach(x -> {
+                // ☠️ Beware of the String/DOM manipulation filth that follows ☠️
+                // Column 1 contains post type (OFFER/WANTED), date/time, post id
+                String column1 = x.child(0).text();
+                String[] splitCol1 = column1.split(" ");
+                String type = splitCol1[1];
+                String datetime = column1.substring((splitCol1[0].length() + splitCol1[1].length() + 2),
+                        column1.length() - (splitCol1[7].length() + 1));
+                long postId = Long.parseLong(
+                        splitCol1[splitCol1.length - 1].substring(2, splitCol1[splitCol1.length - 1].length() - 1));
+                // Column two contains the title and location
+                String column2 = x.child(1).text();
+                String title = x.child(1).child(0).text();
+                String location = column2.substring(title.length() + 2, column2.length() - 13);
+
+                Post p = new Post();
+                if (type.equals("OFFER")) {
+                    p.setType(Post.Type.OFFER);
+                } else {
+                    p.setType(Post.Type.WANTED);
+                }
+                p.setId(postId);
+                p.setTitle(title);
+                p.setLocation(location);
+
+                try {
+                    p.setDate(formatter.parse(datetime));
+                } catch (java.text.ParseException ex) {
+                    // Don't care atm
+                }
+                
+                System.out.println(p);
+            });
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -34,7 +66,7 @@ public class App {
             System.err.println("Usage: jfreecycle [GroupID]\n    Example: jfreecycle GalwayIE");
             System.exit(1);
         }
-        
+
         new App(args[0]);
     }
 }
