d32a22ccff65109b79f819cc60c0a1bbcb4ab33f
Paul Kenny <paul.s.kenny@gmail.com>
2020-08-19 20:49:40 +0100

    ➕ Add post type argument handling

--- src/main/java/io/github/pskenny/jfreecycle/App.java
+++ src/main/java/io/github/pskenny/jfreecycle/App.java
@@ -3,6 +3,7 @@ package io.github.pskenny.jfreecycle;
 import java.util.Collection;
 
 import io.github.pskenny.libjfreecycle.model.Post;
+
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
@@ -13,23 +14,41 @@ import net.sourceforge.argparse4j.inf.Namespace;
  * G'luck.
  */
 public class App {
-    public enum Type {WANTED, OFFER, ALL}
 
     public App(String groupId) {
-        Collection<Post> posts = io.github.pskenny.libjfreecycle.util.PostUtil.getPosts(groupId, Post.Type.ALL);
+        this(groupId, Post.Type.ALL);
+    }
+
+    public App(String groupId, Post.Type type) {
+        Collection<Post> posts = io.github.pskenny.libjfreecycle.util.PostUtil.getPosts(groupId, type);
         posts.forEach(System.out::println);
     }
 
     public static void main(String[] args) {
-        ArgumentParser parser = ArgumentParsers.newFor("jfreecycle").build()
-                .description("Freecycle.org scraper.");
-        parser.addArgument("groupid")
-                .metavar("groupid")
-                .type(String.class)
-                .help("Freecycle group ID");
+        ArgumentParser parser = ArgumentParsers.newFor("jfreecycle").build().description("Freecycle.org scraper.");
+        parser.addArgument("groupid").metavar("GROUPID").type(String.class).required(true).help("Freecycle group ID");
+        parser.addArgument("-t", "-type").metavar("TYPE").type(String.class)
+                .help("Enter \"offer\" or \"wanted\"");
+
         try {
             Namespace res = parser.parseArgs(args);
-            new App(res.getString("groupid"));
+            String groupIdArgument = res.getString("groupid");
+            Post.Type type = Post.Type.ALL;
+            String typeArgument = res.getString("t");
+
+            // Handle post type argument
+            if (typeArgument != null){
+                if (typeArgument.equals("offer")) {
+                    type = Post.Type.OFFER;
+                } else if (typeArgument.equals("wanted")) {
+                    type = Post.Type.WANTED;
+                } else {
+                    parser.printUsage();
+                    return;
+                }
+            }
+
+            new App(groupIdArgument, type);
         } catch (ArgumentParserException e) {
             parser.handleError(e);
         }
